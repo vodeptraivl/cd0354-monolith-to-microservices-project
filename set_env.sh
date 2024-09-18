98a8b0e5a7d10d6646fbe270c021c0c86b55cd5c
VOLA <admin@MacBook-Pro-cua-Eudy.local>
2024-09-18 21:39:16 +0700

    init

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=vola
+export POSTGRES_PASSWORD=Vodeptrai123
+export POSTGRES_HOST=cdr.ckodc1qs7baz.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::volaproject3
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
