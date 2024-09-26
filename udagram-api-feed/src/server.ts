d2b1db8bb63f2a66f348342b71af53fc078c11a5
vola <vodeptraivl@gmail.com>
2024-09-26 10:53:43 +0700

    fix

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -1,50 +1,44 @@
-import cors from 'cors';
-import express from 'express';
-import {sequelize} from './sequelize';
+import cors from "cors";
+import express from "express";
 
-import {IndexRouter} from './controllers/v0/index.router';
+import { IndexRouter } from "./controllers/v0/index.router";
 
-import bodyParser from 'body-parser';
-import {config} from './config/config';
-import {V0_FEED_MODELS} from './controllers/v0/model.index';
-
+import bodyParser from "body-parser";
+import { config } from "./config/config";
+import { sequelize } from "./sequelize";
+import { V0_FEED_MODELS } from "./controllers/v0/model.index";
 
 (async () => {
-  await sequelize.addModels(V0_FEED_MODELS);
+    await sequelize.addModels(V0_FEED_MODELS);
+    await sequelize.sync();
+    console.debug("Initialize database connection...");
+    const app = express();
+    const port = process.env.PORT || 8080;
 
-  console.debug("Initialize database connection...");
-  await sequelize.sync();
+    app.use(bodyParser.json());
 
-  const app = express();
-  const port = process.env.PORT || 8080;
+    // We set the CORS origin to * so that we don't need to
+    // worry about the complexities of CORS this lesson. It's
+    // something that will be covered in the next course.
+    app.use(
+        cors({
+            allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Authorization"],
+            methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+            preflightContinue: true,
+            origin: "*",
+        })
+    );
 
-  app.use(bodyParser.json());
+    app.use("/api/v0/", IndexRouter);
 
-  // We set the CORS origin to * so that we don't need to
-  // worry about the complexities of CORS this lesson. It's
-  // something that will be covered in the next course.
-  app.use(cors({
-    allowedHeaders: [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization',
-    ],
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    origin: '*',
-  }));
+    // Root URI call
+    app.get("/", async (req, res) => {
+        res.send("/api/v0/");
+    });
 
-  app.use('/api/v0/', IndexRouter);
-
-  // Root URI call
-  app.get( '/', async ( req, res ) => {
-    res.send( '/api/v0/' );
-  } );
-
-
-  // Start the Server
-  app.listen( port, () => {
-    console.log( `server running ${config.url}` );
-    console.log( `press CTRL+C to stop server` );
-  } );
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running ${config.url}`);
+        console.log(`press CTRL+C to stop server`);
+    });
 })();
---
+++ udagram-api-feed/www/sequelize.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sequelize = void 0;
+const sequelize_typescript_1 = require("sequelize-typescript");
+const config_1 = require("./config/config");
+exports.sequelize = new sequelize_typescript_1.Sequelize({
+    username: config_1.config.username,
+    password: config_1.config.password,
+    database: config_1.config.database,
+    host: config_1.config.host,
+    dialect: config_1.config.dialect,
+    storage: ":memory:",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+});
+//# sourceMappingURL=sequelize.js.map
